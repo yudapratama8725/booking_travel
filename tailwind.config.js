0036d7bb22abbe0ef652e3a86f06a265ae915801
yudapratama8725 <iptmasyudapratama8725@gmail.com>
2024-05-07 20:07:15 +0700

    [init] : initial commit and setup project

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        "inter" : ['inter']
+      },
+      colors : {
+        "ungu" : '#5D50C6',
+        "pink" : '#F85E9F',
+        "orange" : '#FF5722'
+      }
+    },
+  },
+  plugins: [],
+}
+
